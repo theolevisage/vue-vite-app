88f86e957358305d7fbcf3a31daa61078251b76c
theolevisage <theo.levisage@tutanota.com>
2026-01-02 16:48:49 +0100

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="flex w-full min-h-screen justify-center items-center bg-green-300">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" alt="Vue logo" />
-    </a>
+    <IncrementButton />
   </div>
 </template>
 
 <script setup>
+  import IncrementButton from './components/IncrementButton.vue';
 </script>
---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button 
+        type="button"
+        class="border p-2 rounded bg-gray-100 hover:bg-gray-200 cursor-pointer"
+    >
+        {{ buttonText }}
+    </button >
+</template>
+
+<script setup lang="js">
+import { ref } from 'vue'
+
+const buttonText = ref('Je suis un bouton')
+</script>
